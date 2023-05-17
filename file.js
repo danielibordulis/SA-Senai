eb37954598cce2cab65999ae2af38571f136db0a
Danieli Bordulis <133906565+danielibordulis@users.noreply.github.com>
2023-05-17 15:00:17 -0300

    Commit da S.A
    Hello Word.

---
+++ file.js
@@ -0,0 +1,25 @@
+let A,B,C,D
+let opcaoUsuario
+
+
+
+opcaoUsuario=prompt("Escolha a opção desejada ")
+
+
+switch (opcaoUsuario){
+    case 'A':
+
+     alert ("Falar com a atendente")
+     break
+
+    case 'B':
+            alert("Falar com o RH")
+            break
+    case 'C':
+            alert("Falar com o gerente")
+            break
+
+    case 'D':
+        alert ("Sair")
+    
+}
